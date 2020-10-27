f134642f71fb8670b020d571241a7bfc70054b18
Robbie Blaine <robbie@zenaptix.com>
2020-10-27 10:38:25 +0200

    Add support for git urls as dependency repositories
    Signed-off-by: Jeff Valore <rally25rs@yahoo.com>
    
    fix 'helm dep build' command when chart contains 'git:' dependencies
    
    Signed-off-by: Jeff Valore <rally25rs@yahoo.com>
    
    fix lint error
    
    Signed-off-by: Jeff Valore <rally25rs@yahoo.com>
    
    Conflicts:
    	internal/resolver/resolver.go
    
    Add support for git urls as dependency repositories
    
    Signed-off-by: Jeff Valore <rally25rs@yahoo.com>
    
    fix 'helm dep build' command when chart contains 'git:' dependencies
    
    Signed-off-by: Jeff Valore <rally25rs@yahoo.com>
    
    fix lint error
    
    Signed-off-by: Jeff Valore <rally25rs@yahoo.com>

--- internal/resolver/resolver.go
+++ internal/resolver/resolver.go
@@ -18,6 +18,7 @@ package resolver
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -28,10 +29,13 @@ import (
 
 	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/chart/loader"
+	"helm.sh/helm/v3/pkg/gitutil"
 	"helm.sh/helm/v3/pkg/helmpath"
 	"helm.sh/helm/v3/pkg/provenance"
 	"helm.sh/helm/v3/pkg/repo"
 )
+
+var gitGetRefs = gitutil.GetRefs
 
 // Resolver resolves dependencies from semantic version ranges to a particular version.
 type Resolver struct {
@@ -88,6 +92,29 @@ func (r *Resolver) Resolve(reqs []*chart.Dependency, repoNames map[string]string
 			}
 			continue
 		}
+
+		if strings.HasPrefix(d.Repository, "git:") {
+			refs, err := gitGetRefs(strings.TrimPrefix(d.Repository, "git:"))
+
+			if err != nil {
+				return nil, err
+			}
+
+			_, found := refs[d.Version]
+
+			if !found {
+				return nil, fmt.Errorf(`dependency %q is missing git branch or tag: %s.
+			When using a "git:" type repository, the "version" should be a valid branch or tag name`, d.Name, d.Version)
+			}
+
+			locked[i] = &chart.Dependency{
+				Name:       d.Name,
+				Repository: d.Repository,
+				Version:    d.Version,
+			}
+			continue
+		}
+
 		constraint, err := semver.NewConstraint(d.Version)
 		if err != nil {
 			return nil, errors.Wrapf(err, "dependency %q has an invalid version/constraint format", d.Name)
